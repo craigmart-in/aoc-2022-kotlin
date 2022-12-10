5fdc0530c5f3a21ee5d2570a8e3110aedc92062e
Craig Martin <me@craigmart.in>
2022-12-10 10:25:13 -0600

    Solve day 8 part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -2,21 +2,15 @@ fun main() {
     fun part1(input: List<String>): Int {
         val height = input.size
         val width = input[0].length
-        val treeGrid = Array(width) {Array(height) {0} }
-
-        // Parse input into 2d array
-        for ((y, row) in input.withIndex()) {
-            for ((x, value) in row.toCharArray().withIndex()) {
-                treeGrid[y][x] = value.digitToInt()
-            }
-        }
+        val treeGrid = getTreeGrid(input)
 
         // Initialize visible count to outside trees
         var visisbleCount: Int = (width * 2) + ((height - 2) * 2)
 
         for (y in 1 until height - 1) {
             for (x in 1 until width - 1) {
-                if (isTreeVisible(x, y, treeGrid)) {
+                val treesInLineOfSight = getTreesInLineOfSight(x, y, treeGrid)
+                if (isTreeVisible(treeGrid[y][x], treesInLineOfSight)) {
                     visisbleCount++
                 }
             }
@@ -26,19 +20,53 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val height = input.size
+        val width = input[0].length
+        val treeGrid = getTreeGrid(input)
+
+        // Initialize visible count to outside trees
+        var largestScenicSore = 0
+
+        for (y in 1 until height - 1) {
+            for (x in 1 until width - 1) {
+                val treesInLineOfSight = getTreesInLineOfSight(x, y, treeGrid)
+                val scenicScore = getScenicScore(treeGrid[y][x], treesInLineOfSight)
+
+                if (scenicScore > largestScenicSore) {
+                    largestScenicSore = scenicScore
+                }
+            }
+        }
+
+        return largestScenicSore
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 21)
+    check(part2(testInput) == 8)
 
     val input = readInput("Day08")
     println(part1(input))
     println(part2(input))
 }
 
-fun isTreeVisible(x: Int, y: Int, treeGrid: Array<Array<Int>>) : Boolean {
+fun getTreeGrid(input: List<String>) : Array<Array<Int>> {
+    val height = input.size
+    val width = input[0].length
+    val treeGrid = Array(width) {Array(height) {0} }
+
+    // Parse input into 2d array
+    for ((y, row) in input.withIndex()) {
+        for ((x, value) in row.toCharArray().withIndex()) {
+            treeGrid[y][x] = value.digitToInt()
+        }
+    }
+
+    return treeGrid
+}
+
+fun getTreesInLineOfSight(x: Int, y: Int, treeGrid: Array<Array<Int>>) : TreesInLineOfSight {
     val currentTreeHeight = treeGrid[y][x]
     val treesInRow = treeGrid[y]
 
@@ -46,52 +74,66 @@ fun isTreeVisible(x: Int, y: Int, treeGrid: Array<Array<Int>>) : Boolean {
     val treesToLeft = treesInRow.take(x)
     val treesToRight = treesInRow.takeLast(treesInRow.size - x - 1)
 
-    // If there are no trees to the left of the tree then it is visible
-    if (treesToLeft.none { it >= currentTreeHeight }) {
-        return true
-    }
-
-    // If there are no trees to the right of the tree then it is visible
-    if (treesToRight.none { it >= currentTreeHeight}) {
-        return true
-    }
-
     val treesAbove = mutableListOf<Int>()
     treeGrid.take(y).forEach { treesAbove.add(it[x]) }
 
     val treesBelow = mutableListOf<Int>()
     treeGrid.takeLast(treeGrid.size - y - 1).forEach { treesBelow.add(it[x]) }
 
+    return TreesInLineOfSight(treesToLeft, treesToRight, treesAbove, treesBelow)
+}
+
+fun isTreeVisible(currentTreeHeight: Int, treesInLineOfSight: TreesInLineOfSight) : Boolean {
+    // If there are no trees to the left of the tree then it is visible
+    if (treesInLineOfSight.treesToLeft.none { it >= currentTreeHeight }) {
+        return true
+    }
+
+    // If there are no trees to the right of the tree then it is visible
+    if (treesInLineOfSight.treesToRight.none { it >= currentTreeHeight}) {
+        return true
+    }
+
     // If there are no trees to above the tree then it is visible
-    if (treesAbove.none { it >= currentTreeHeight }) {
+    if (treesInLineOfSight.treesAbove.none { it >= currentTreeHeight }) {
         return true
     }
 
     // If there are no trees below the tree then it is visible
-    if (treesBelow.none { it >= currentTreeHeight}) {
+    if (treesInLineOfSight.treesBelow.none { it >= currentTreeHeight}) {
         return true
     }
 
     return false
 }
 
-/*
+fun getScenicScore(currentTreeHeight: Int, treesInLineOfSight: TreesInLineOfSight) : Int {
+    var leftCount = 0
+    var rightCount = 0
+    var aboveCount = 0
+    var belowCount = 0
 
-30373
+    for (tree in treesInLineOfSight.treesToLeft.reversed()) {
+        leftCount++
+        if (tree >= currentTreeHeight) break
+    }
 
+    for (tree in treesInLineOfSight.treesToRight) {
+        rightCount++
+        if (tree >= currentTreeHeight) break
+    }
 
-Current tree is 7
-All trees >= 7 -> none
-This means the tree is visible
+    for (tree in treesInLineOfSight.treesAbove.reversed()) {
+        aboveCount++
+        if (tree >= currentTreeHeight) break
+    }
 
-Current tree is 3rd with height 3
-All trees >= 3 -> left (3); right (7, 3)
-Not visible
+    for (tree in treesInLineOfSight.treesBelow) {
+        belowCount++
+        if (tree >= currentTreeHeight) break
+    }
 
+    return leftCount * rightCount * aboveCount * belowCount
+}
 
-33549
-
-Current tree = 5
-All trees >= 5 -> left(); right(9)
-visible on left but not right
- */
+data class TreesInLineOfSight(val treesToLeft: List<Int>, val treesToRight: List<Int>, val treesAbove: List<Int>, val treesBelow: List<Int>)
