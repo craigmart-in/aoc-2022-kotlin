8cd935bab867b521ecd23858efad32ba169aabe9
Craig Martin <me@craigmart.in>
2022-12-11 11:04:12 -0600

    Solve day 10 part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -19,28 +19,59 @@ fun main() {
 
         for (i in operations.indices) {
             val cycle = i + 1
-            println("Start of $cycle cycle x is $x")
+//            println("Start of $cycle cycle x is $x")
             if (cycle == nextInterestingSignal) {
                 val signalStrength = (cycle * x)
-                println("During the $cycle cycle, the signal strength is ")
+//                println("During the $cycle cycle, the signal strength is ")
                 signalStrengthSum += signalStrength
                 nextInterestingSignal += 40
             }
 
             if (operations[i].startsWith("addx")) {
                 val addition = operations[i].split(" ")[1].toInt()
-                println("Adding $addition to $x")
+//                println("Adding $addition to $x")
                 x += addition
             }
 
-            println("After $cycle cycle x is $x")
+//            println("After $cycle cycle x is $x")
         }
 
         return signalStrengthSum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun getPixel(spritePosition: Int, index: Int): String {
+        val spritePositions = listOf<Int>(spritePosition - 1, spritePosition, spritePosition + 1)
+        if (spritePositions.contains(index % 40)) {
+            return "#"
+        }
+        return "."
+    }
+
+    fun part2(input: List<String>): String {
+        val operations = parseInput(input)
+        val crt = StringBuilder()
+        var crtWidth = 40
+        var crtRow = 0
+        var spritePosition = 1
+
+        for (i in operations.indices) {
+            val cycle = i + 1
+
+            crt.append(getPixel(spritePosition, i))
+
+            if (cycle == crtWidth) {
+                crt.appendLine()
+                crtRow++
+                crtWidth += 40
+            }
+
+            if (operations[i].startsWith("addx")) {
+                val movement = operations[i].split(" ")[1].toInt()
+                spritePosition += movement
+            }
+        }
+
+        return crt.toString()
     }
 
     // test if implementation meets criteria from the description, like:
