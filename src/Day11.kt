853a4f998d4226a685fb5551f028965f37646a46
Craig Martin <me@craigmart.in>
2022-12-11 19:22:48 -0600

    Solve day 11 part 2 with help

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,13 +1,11 @@
 fun main() {
-    data class Monkey(val worryLevels: MutableList<Int>, val operation: String, val test: Int, val nextMonkeyIfTrue: Int, val nextMonkeyIfFalse: Int, var inspectCount: Int = 0)
+    data class Monkey(val worryLevels: MutableList<ULong>, val operation: String, val test: Int, val nextMonkeyIfTrue: Int, val nextMonkeyIfFalse: Int, var inspectCount: Long = 0)
 
     fun getMonkeys(input: List<String>): List<Monkey> {
         var monkeys = mutableListOf<Monkey>()
-        var currentMonkey: Monkey
-
 
         for (index in input.indices step 7) {
-            val startingItems = input[index+1].removePrefix("  Starting items: ").split(", ").map { it.toInt() }
+            val startingItems = input[index+1].removePrefix("  Starting items: ").split(", ").map { it.toULong() }
             val operation = input[index+2].removePrefix("  Operation: new = ")
             val test = input[index+3].removePrefix("  Test: divisible by ").toInt()
             val ifTrue = input[index+4].removePrefix("    If true: throw to monkey ").toInt()
@@ -19,67 +17,61 @@ fun main() {
         return monkeys
     }
 
-    fun inspectItem(currentWorryLevel: Int, operation: String): Int {
-        var newWorryLevel = 0
+    fun inspectItem(currentWorryLevel: ULong, operation: String): ULong {
+        var newWorryLevel = 0UL
         var typeOfOperation = ""
-        var amount = 0
+        var amount = 0UL
 
         val tokens = operation.split(" ")
 
         amount = if (tokens[2].toIntOrNull() == null) {
             currentWorryLevel
         } else {
-            tokens[2].toInt()
+            tokens[2].toULong()
         }
 
         newWorryLevel = when(tokens[1]) {
             "*" -> {
-                typeOfOperation = "multiplied"
-                return currentWorryLevel * amount
-            }
-            "/" -> {
-                typeOfOperation = "divisible"
-                return currentWorryLevel / amount
-            }
-            "+" -> {
-                typeOfOperation = "increases"
-                return currentWorryLevel + amount
+                typeOfOperation = "is multiplied"
+                currentWorryLevel * amount
             }
             else -> {
-                typeOfOperation = "decreases"
-                return currentWorryLevel - amount
+                typeOfOperation = "increases"
+                currentWorryLevel + amount
             }
         }
 
-        println("    Worry level is $typeOfOperation by $amount to $newWorryLevel")
+        println("    Worry level $typeOfOperation by $amount to $newWorryLevel.")
         return newWorryLevel
     }
 
-    fun testItem(worryLevel: Int, test: Int): Boolean {
-        if (worryLevel % test == 0) {
-            println("   Current worry level is divisible by $test")
+    fun testItem(worryLevel: ULong, test: Int): Boolean {
+        if (worryLevel % test.toULong() == 0UL) {
+            println("    Current worry level is divisible by $test.")
             return true
         }
-        println("   Current worry level is not divisible by $test")
+        println("    Current worry level is not divisible by $test.")
         return false
     }
 
-    fun part1(input: List<String>): Int {
-        val monkeys = getMonkeys(input)
-
-        for (round in 0 until 20) {
+    fun simulateRounds(monkeys: List<Monkey>, count: Int, isWorried: Boolean, modulus: ULong = 1UL) {
+        for (round in 0 until count) {
             for ((index, monkey) in monkeys.withIndex()) {
                 println("Monkey $index:")
                 for (worryLevel in monkey.worryLevels) {
                     println("  Monkey inspects an item with a worry level of $worryLevel.")
-                    val newWorryLevel: Int = inspectItem(worryLevel, monkey.operation) / 3
+                    var newWorryLevel: ULong = inspectItem(worryLevel, monkey.operation)
+                    if (isWorried) {
+                        newWorryLevel %= modulus
+                    } else {
+                        newWorryLevel /= 3UL
+                        println("    Monkey gets bored with item. Worry level is divided by 3 to $newWorryLevel.")
+                    }
 
-                    println("    Monkey gets bored with item. Worry level is divided by 3 to $newWorryLevel")
+                    val divisible = testItem(newWorryLevel, monkey.test)
+                    val nextMonkey = if (divisible) monkey.nextMonkeyIfTrue else monkey.nextMonkeyIfFalse
 
-                    val divisble = testItem(newWorryLevel, monkey.test)
-                    val nextMonkey = if (divisble) monkey.nextMonkeyIfTrue else monkey.nextMonkeyIfFalse
-
-                    println("    Item with worry level $newWorryLevel is thrown to monkey $nextMonkey")
+                    println("    Item with worry level $newWorryLevel is thrown to monkey $nextMonkey.")
                     monkeys[nextMonkey].worryLevels.add(newWorryLevel)
                 }
                 monkey.inspectCount += monkey.worryLevels.size
@@ -87,7 +79,7 @@ fun main() {
             }
 
             println()
-            println("After round ${round+1}, the monkeys are holding items with these worry levels:")
+            println("After round ${round + 1}, the monkeys are holding items with these worry levels:")
             for ((index, monkey) in monkeys.withIndex()) {
                 print("Monkey $index: ")
                 for ((index, item) in monkey.worryLevels.withIndex()) {
@@ -100,19 +92,37 @@ fun main() {
                 println()
             }
         }
+    }
+
+    fun part1(input: List<String>): Long {
+        val monkeys = getMonkeys(input)
+
+        simulateRounds(monkeys, 20, false)
 
         val sortedMonkeys = monkeys.sortedByDescending { it.inspectCount }
 
         return sortedMonkeys[0].inspectCount * sortedMonkeys[1].inspectCount
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val monkeys = getMonkeys(input)
+
+        var modulus = 1UL
+        for (monkey in monkeys) {
+            modulus *= monkey.test.toULong()
+        }
+
+        simulateRounds(monkeys, 10000, true, modulus)
+
+        val sortedMonkeys = monkeys.sortedByDescending { it.inspectCount }
+
+        return (sortedMonkeys[0].inspectCount * sortedMonkeys[1].inspectCount)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test")
-    check(part1(testInput) == 10605)
+    check(part1(testInput) == 10605L)
+    check(part2(testInput) == 2713310158)
 
     val input = readInput("Day11")
     println(part1(input))
